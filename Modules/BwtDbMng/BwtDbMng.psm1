ece17c5bc9a0dcc3cdcdee3a9513595b306a86ec
Chris Ashton <chris@blueprintwebtech.com>
2013-07-19 13:19:34 +0100

    Add holding database restore functions.

--- Modules/BwtDbMng/BwtDbMng.psm1
+++ Modules/BwtDbMng/BwtDbMng.psm1
@@ -64,4 +64,117 @@ function DbBackup($database, $targetDir, $targetFile)
     #See more at: http://www.sswug.org/articlesection/default.aspx?TargetID=44909#sthash.YMwxs7lz.dpuf
 }
 
-function
+function DbRestore()
+{
+    #============================================================
+    # Restore a Database using PowerShell and SQL Server SMO
+    # Restore to the same database, overwrite existing db
+    # http://www.sswug.org/articlesection/default.aspx?TargetID=44909
+    #============================================================
+     
+    #clear screen
+    cls
+     
+    #load assemblies
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | Out-Null
+
+    #Need SmoExtended for backup
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SmoExtended") | Out-Null
+    [Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.ConnectionInfo") | Out-Null
+    [Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SmoEnum") | Out-Null
+     
+    #get backup file
+    #you can also use PowerShell to query the last backup file based on the timestamp
+    #I'll save that enhancement for later
+    $backupFile = "C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\Backup\brit-thoracic.local.bak"
+                  
+    #we will query the db name from the backup file later
+     
+    $server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") "(local)"
+    $backupDevice = New-Object ("Microsoft.SqlServer.Management.Smo.BackupDeviceItem") ($backupFile, "File")
+    $smoRestore = new-object("Microsoft.SqlServer.Management.Smo.Restore")
+     
+    #settings for restore
+    $smoRestore.NoRecovery = $false;
+    $smoRestore.ReplaceDatabase = $true;
+    $smoRestore.Action = "Database"
+     
+    #show every 10% progress
+    $smoRestore.PercentCompleteNotification = 10;
+     
+    $smoRestore.Devices.Add($backupDevice)
+     
+    #read db name from the backup file's backup header
+    $smoRestoreDetails = $smoRestore.ReadBackupHeader($server)
+     
+    #display database name
+    "Database Name from Backup Header : " + $smoRestoreDetails.Rows[0]["DatabaseName"]
+     
+    $smoRestore.Database = $smoRestoreDetails.Rows[0]["DatabaseName"]
+     
+    #restore
+    $smoRestore.SqlRestore($server)
+     
+    "Done"
+    #See more at: http://www.sswug.org/articlesection/default.aspx?TargetID=44909#sthash.YMwxs7lz.dpuf
+}
+
+function DbRestoreNewName()
+{
+    #============================================================
+    # Restore a Database using PowerShell and SQL Server SMO
+    # Restore to the a new database name, specifying new mdf and ldf
+    # http://www.sswug.org/articlesection/default.aspx?TargetID=44909
+    #============================================================
+     
+    #clear screen
+    cls
+     
+    #load assemblies
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | Out-Null
+
+    #Need SmoExtended for backup
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SmoExtended") | Out-Null
+    [Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.ConnectionInfo") | Out-Null
+    [Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SmoEnum") | Out-Null
+     
+    $backupFile = 'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\Backup\brit-thoracic.local.bak'
+     
+    #we will query the database name from the backup header later
+    $server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") "(local)"
+    $backupDevice = New-Object("Microsoft.SqlServer.Management.Smo.BackupDeviceItem") ($backupFile, "File")
+    $smoRestore = new-object("Microsoft.SqlServer.Management.Smo.Restore")
+     
+    #restore settings
+    $smoRestore.NoRecovery = $false;
+    $smoRestore.ReplaceDatabase = $true;
+    $smoRestore.Action = "Database"
+    $smoRestorePercentCompleteNotification = 10;
+    $smoRestore.Devices.Add($backupDevice)
+     
+    #get database name from backup file
+    $smoRestoreDetails = $smoRestore.ReadBackupHeader($server)
+     
+    #display database name
+    "Database Name from Backup Header : " +$smoRestoreDetails.Rows[0]["DatabaseName"]
+     
+    #give a new database name
+    $smoRestore.Database =$smoRestoreDetails.Rows[0]["DatabaseName"] + "_Copy"
+    $smoRestore.Database = "new-database-name" 
+     
+    #specify new data and log files (mdf and ldf)
+    $smoRestoreFile = New-Object("Microsoft.SqlServer.Management.Smo.RelocateFile")
+    $smoRestoreLog = New-Object("Microsoft.SqlServer.Management.Smo.RelocateFile")
+     
+    #the logical file names should be the logical filename stored in the backup media
+    $smoRestoreFile.LogicalFileName = $smoRestoreDetails.Rows[0]["DatabaseName"]
+    $smoRestoreFile.PhysicalFileName = $server.Information.MasterDBPath + "\" + $smoRestore.Database + "_Data.mdf"
+    $smoRestoreLog.LogicalFileName = $smoRestoreDetails.Rows[0]["DatabaseName"] + "_Log"
+    $smoRestoreLog.PhysicalFileName = $server.Information.MasterDBLogPath + "\" + $smoRestore.Database + "_Log.ldf"
+    $smoRestore.RelocateFiles.Add($smoRestoreFile)
+    $smoRestore.RelocateFiles.Add($smoRestoreLog)
+     
+    #restore database
+    $smoRestore.SqlRestore($server)
+    # See more at: http://www.sswug.org/articlesection/default.aspx?TargetID=44909#sthash.YMwxs7lz.dpuf
+}
