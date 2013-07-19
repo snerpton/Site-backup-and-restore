0b77d81e4d10a266fdf465779284480742513ed3
Dev server <Dev server@localhost>
2013-07-19 16:40:34 +0000

    Add restore files and database into restore routine.

--- siteManage.ps1
+++ siteManage.ps1
@@ -135,12 +135,12 @@ function CreateLocalBackup()
 
 function RestoreLocalBackupToRemote()
 {
-    #RestoreFiles
+    RestoreFiles
     Write-Host "Unzipping database backup..."
     Write-Host "--> src:  $restoreDbSrcDbZip"
     Write-Host "--> dest: $restoreDbTargetDir"
     UnZipMe -zipfilename $restoreDbSrcDbZip -destination $restoreDbTargetDir
-    #RestoreDb -dbSrcFileDir $restoreDbTargetDir -dbSrcFileBak $bkupDbTargetFile -dbNewName $restoreDbNewName
+    RestoreDb -dbSrcFileDir $restoreDbTargetDir -dbSrcFileBak $bkupDbTargetFile -dbNewName $restoreDbNewName
 }
 
 cls
